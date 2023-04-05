b0aac538207df00741196890bc7b79c5c5120181
Michael Sarrazin <michael.sarrazin@webedia-group.com>
2023-04-05 11:01:21 +0200

    create fiche restaurant avec produits par restaurant

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\RestaurantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,6 +20,23 @@ class RestaurantController extends AbstractController
         // Affichage de la vue
         return $this->render('restaurant/index.html.twig', [
             'donnees' => $donnees
+        ]);
+    }
+
+    #[Route('/restaurants/show/{slug}', name: 'restaurants/{slug}')]
+    public function show(string $slug, RestaurantRepository $repository, EntityManagerInterface $doctrine): Response
+    {
+        //$entityManager = $doctrine->getManager();
+        //$announce = $doctrine->getRepository(Announce::class)->find($id);
+        $restaurant = $repository->findOneBy(['slug' => $slug]);
+
+        if (!$restaurant) {
+            throw $this->createNotFoundException("Le restaurant n'existe pas");
+        }
+        
+        return $this->render('restaurant/show.html.twig', [
+            'restaurant' => $restaurant,
+            'slug' => $restaurant->getSlug(),
         ]);
     }
 }
